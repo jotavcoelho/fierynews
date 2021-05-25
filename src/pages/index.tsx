899e5bc5cdba2c6454537a93bfdd3ba2a83f53cb
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2021-05-25 20:05:25 -0300

    home page fully styled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss'; 
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home | fierynews</title>
       </Head>
-      <h1>
-        Hello modafoca
-      </h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all posts <br />
+            <span>for $9.90 a month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Coding girl" />
+      </main>
     </>
   )
 }
