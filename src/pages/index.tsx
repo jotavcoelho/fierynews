410c5f7f10e5b71ab1c0981c2f8643bd9efdd625
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2021-05-24 22:15:35 -0300

    set global styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
-import styles from '../styles/home.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello modafoca</h1>
+    <>
+      <Head>
+        <title>Home | fierynews</title>
+      </Head>
+      <h1>
+        Hello modafoca
+      </h1>
+    </>
   )
 }
