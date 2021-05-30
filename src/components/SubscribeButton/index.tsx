0220f838b639ca90d1a65a0c17c50e02cad3b365
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2021-05-30 18:59:01 -0300

    successfully redirecting to stripeCheckout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -9,13 +11,23 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
 
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn('github');
       return;
     }
 
-    // creation of checkout session
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
   }
 
   return (
