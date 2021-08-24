08a8059e711442c263e23adc1ac625ddb87c3e33
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2021-08-24 19:41:38 -0300

    testing home pt2, getstaticprops

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import Home from '../../pages';
+import { stripe } from '../../services/stripe';
+import Home, { getStaticProps } from '../../pages';
+import { mocked } from 'ts-jest/utils';
 
 jest.mock('next/router');
 jest.mock('next-auth/client', () => {
@@ -8,10 +10,34 @@ jest.mock('next-auth/client', () => {
   }
 });
 
+jest.mock('../../services/stripe');
+
 describe('Home page', () => {
   it('renders correctly', () => {
     render(<Home product={{ priceId: 'fake priceId', amount: '$10,00' }} />)
 
     expect(screen.getByText(/\$10,00/i)).toBeInTheDocument();
+  })
+
+  it('loads initial data', async () => {
+    const mockedStripePricesRetrieve = mocked(stripe.prices.retrieve);
+
+    mockedStripePricesRetrieve.mockResolvedValueOnce({
+      id: 'fakepriceid',
+      unit_amount: 1000
+    } as any);
+
+    const response = await getStaticProps({});
+    
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: 'fakepriceid',
+            amount: '$10.00'
+          }
+        }
+      })
+    )
   })
 })
